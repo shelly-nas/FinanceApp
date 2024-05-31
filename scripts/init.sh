b787233772096d8daad36bd4827faeebddc06240
Jelle <jellevandergraaf@live.com>
2024-05-31 09:51:47 +0200

    Update init file

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,38 +1,57 @@
 #!/bin/bash
-# Install dependencies
-cd client
+
+# Save the current working directory
+ORIG_DIR=$(pwd)
+
+# Change to $WORKDIR/client and server
+cd $WORKDIR/client
+
+# Install dependencies in client
+echo "Installing client dependencies..."
 npm install
 
-# Create .env file in client directory
+# Remove the .env file if it exists
+sudo rm -f .env
+echo "Existing client .env file removed."
+
+# Create new .env file in client directory
 CLIENT_ENV_FILE=".env"
 {
     echo "VITE_BASE_URL=http://${HOST}:${PORT}"
-} > $CLIENT_ENV_FILE
+} > $CLIENT_ENV
 
-echo ".env file generated successfully in client directory!"
+echo "Client .env file generated successfully!"
 
-# Install dependencies
-cd ../server
+# Change to server directory
+cd $WORKDIR/server
+
+# Install dependencies in server
+echo "Installing server dependencies..."
 npm install
 
 # Compile TypeScript
+echo "Compiling TypeScript..."
 npm run build
 
 # Remove the .env file if it exists
 sudo rm -f .env
+echo "Existing server .env file removed."
 
-# Define the path to the .env file
+# Define the path to the .env file in server directory
 SERVER_ENV_FILE=".env"
 
 # Write the environment variables to the .env file in server directory
 {
-    echo "DB_USER=${DB_USER}"
-    echo "DB_PASSWORD=${DB_PASSWORD}"
-    echo "DB_HOST=${HOST}
-    echo "DB_PORT=${DB_PORT}"
-    echo "DB_NAME=${DB_NAME}"
-    echo "TABLE_NAME=${TABLE_NAME}"
+    echo "DB_USER=${DBUSER}"
+    echo "DB_PASSWORD=${DBPASSWORD}"
+    echo "DB_HOST=${HOST}"
+    echo "DB_PORT=${DBPORT}"
+    echo "DB_NAME=${DBNAME}"
+    echo "TABLE_NAME=${TABLENAME}"
     echo "SERVER_PORT=${PORT}"
 } > $SERVER_ENV_FILE
 
-echo ".env file generated successfully in server directory!"
+echo "Server .env file generated successfully!"
+
+# Return to original directory
+cd $ORIG_DIR
