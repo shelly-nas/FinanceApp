54cbb73085275a5db6cf22e9f17e2f93afa05103
Jelle <jellevandergraaf@live.com>
2024-05-31 10:09:17 +0200

    Update init pwd ref

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 # Save the current working directory
-ORIG_DIR=$(pwd)
+WORKDIR=$(pwd)
 
 # Change to $WORKDIR/client and server
 cd $WORKDIR/client
 
 # Install dependencies in client
-echo "Installing client dependencies..."
+echo "### Installing client dependencies..."
 npm install
 
 # Remove the .env file if it exists
 sudo rm -f .env
-echo "Existing client .env file removed."
+echo "## Existing client .env file removed."
 
 # Create new .env file in client directory
 CLIENT_ENV_FILE=".env"
 {
     echo "VITE_BASE_URL=http://${HOST}:${PORT}"
-} > $CLIENT_ENV
+} > $CLIENT_ENV_FILE
 
-echo "Client .env file generated successfully!"
+echo "## Client .env file generated successfully!"
 
 # Change to server directory
 cd $WORKDIR/server
 
 # Install dependencies in server
-echo "Installing server dependencies..."
+echo "### Installing server dependencies..."
 npm install
 
 # Compile TypeScript
-echo "Compiling TypeScript..."
+echo "## Compiling TypeScript..."
 npm run build
 
 # Remove the .env file if it exists
 sudo rm -f .env
-echo "Existing server .env file removed."
+echo "## Existing server .env file removed."
 
 # Define the path to the .env file in server directory
 SERVER_ENV_FILE=".env"
@@ -51,7 +51,4 @@ SERVER_ENV_FILE=".env"
     echo "SERVER_PORT=${PORT}"
 } > $SERVER_ENV_FILE
 
-echo "Server .env file generated successfully!"
-
-# Return to original directory
-cd $ORIG_DIR
+echo "## Server .env file generated successfully!"
