50dd1af41806d8854c374d6870befbe3d56d1ad1
Jelle <jellevandergraaf@live.com>
2024-05-31 10:56:23 +0200

    Update exec start locations for front and back-end

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,10 +28,10 @@ sudo rm $SERVICE_PATH
 # Determine ExecStart based on the configuration
 case $CONFIG in
     client)
-        EXEC_START="/home/pi/.nvm/versions/node/v20.11.1/bin/node dist/index.js"
+        EXEC_START="/home/pi/.nvm/versions/node/v20.11.1/bin/npx vite"
         ;;
     server)
-        EXEC_START="/home/pi/.nvm/versions/node/v20.11.1/bin/node server/index.js"
+        EXEC_START="/home/pi/.nvm/versions/node/v20.11.1/bin/node dist/index.js"
         ;;
     *)
         echo "Invalid config. Allowed values are: client, server, or plus."
